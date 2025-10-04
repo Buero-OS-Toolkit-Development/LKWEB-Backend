a307bad35be031fd1f77e9838d7db5892520e58b
leander@kafemann.berlin <leander@kafemann.berlin>
2025-10-04 15:03:36 +0200

    fix

--- js/gameScriptKafemann.js
+++ js/gameScriptKafemann.js
@@ -1,8 +1,8 @@
 function removeGame(game) {
-  window.location.href = "https://lkunited.pythonanywhere.com/private/gameRemoving?game=" + game + "&name=" + getCookie("nameGame", document.cookie) + "&password=" + getCookie("passwordGame", document.cookie);
+  window.location.href = "https://lkunited.pythonanywhere.com/private/gameRemoving?game=" + game + "&name=" + getCookie("nameGame", document.cookie) + "&code=" + getCookie("passwordGame", document.cookie);
 }
 
 function addGame() {
     let game = prompt("Welches Spiel moechtest du hinzufuegen?");
-    window.location.href = "https://lkunited.pythonanywhere.com/private/gameAdding?game=" + game + "&name=" + getCookie("nameGame", document.cookie) + "&password=" + getCookie("passwordGame", document.cookie);
+    window.location.href = "https://lkunited.pythonanywhere.com/private/gameAdding?game=" + game + "&name=" + getCookie("nameGame", document.cookie) + "&code=" + getCookie("passwordGame", document.cookie);
 }

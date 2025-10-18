e661edc5cb6fdffb588b94df294f103a58fc087c
leander@kafemann.berlin <leander@kafemann.berlin>
2025-10-18 10:32:12 +0200

    aff coffee resources

---
+++ js/coffeePass.js
@@ -0,0 +1,16 @@
+function checkCoffeePassCookie() {
+    let passID = getCookie("coffeePass", document.cookie);
+    if (passID != null) {
+        window.location.href = "https://lkunited.pythonanywhere.com/private/coffeePass?id=" + passID;
+    } else {
+        alert("Keinen Kaffee-Pass Cookie gefunden.");
+    }
+}
+
+function setCoffeePassCookie(passID) {
+    if (getCookie("coffeePass", document.cookie) == null) {
+        setCookie("coffeePass", passID, 30);
+    } else {
+        renewCookie("coffeePass", 30);
+    }
+}

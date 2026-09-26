08829aef8e5e9fb72f2edc68426ee0a70b3569bd
Leander Kafemann <leander@kafemann.berlin>
2026-09-26 12:18:50 +0200

    outsource sessionid cookie for bugfixing KeyError

--- js/bankMenu.js
+++ js/bankMenu.js
@@ -3,3 +3,12 @@ function logout() {
     let sessionid = getCookie("sessionid");
     window.location.href = "https://lkunited.pythonanywhere.com/bank/logout?sessionid=" + sessionid;
 }
+
+function sessionIdCookie(sessionid) {
+    // sets the sessionid cookie
+    if (getCookie("sessionid", document.cookie) == "") {
+        setCookie("sessionid", sessionid, 7);
+    } else {
+        renewCookie("sessionid", 7)
+    }
+}
